c65aabab2ccce7d9afef0220e706a1e201f3ad5e
AtharvNatu <natuatharv21@gmail.com>
2024-06-20 00:12:07 +0530

    Initial Commit

---
+++ 01-Win32/01-Windowing/01-FirstWindow/Window.c
@@ -0,0 +1,81 @@
+#include <windows.h>
+
+// Global Callback Declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Entry-point Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	// Variable Declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyWindow");
+
+	// Code
+	ZeroMemory((void*)&wndclass, sizeof(WNDCLASSEX));
+	
+	// Initialization of Window Class
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+	// Register above class
+	RegisterClassEx(&wndclass);
+
+	// Create Window
+	hwnd = CreateWindow(
+		szAppName,
+		TEXT("Atharv Natu : First Window"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	// Message Loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	// Code
+	switch (iMsg)
+	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+		break;
+
+		default:
+		break;
+	}
+
+	// Calling Default Window Procedure
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Windows-Native-Development
 Windows OS Specific Development using Win32 SDK, COM and WinRT
+
+
+#For Win32
+
+To Build the Source Code using x64 Native Tools Command Prompt
+
+1. Open the Command Prompt in the source code directory
+2.	To Compile C Code, use the following command
+		cl.exe /c Source.c
+	To Compile C++ Code, use the following command
+		cl.exe /c /EHsc Source.cpp
+3. To Link the code, use the following command
+		link.exe Source.obj user32.lib gdi32.lib kernel32.lib /SUBSYSTEM:WINDOWS
+4. These steps will create the executable application
