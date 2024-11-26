046092c1ef761e6f624bca699bd896ff54400b7c
AtharvNatu <natuatharv21@gmail.com>
2024-11-26 23:46:14 +0530

    Updated Color Picker

--- 03-Projects/Image-Editor/07-ColorPicker/Window.c
+++ 03-Projects/Image-Editor/07-ColorPicker/Window.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include "Window.h"
 
-#define WINDOW_WIDTH 	1920
-#define WINDOW_HEIGHT	1080
+#define WINDOW_WIDTH 	1024
+#define WINDOW_HEIGHT	768
 
 //* Global Callback Declaration
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 //* Global Variable Declrations
-FILE *gpFile_UserLog = NULL, *gpFile_ColorPickerLog = NULL;
+FILE *gpFile_UserLog = NULL, *gpFile_ColorPickerLog = NULL, *gpFile_OGL_Log = NULL;
 
 //* Entry-point Function
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
@@ -72,36 +72,33 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
-	// Variable Declarations
+	//* Function Declarations
+	BOOL createAndOpenLogFiles();
+	BOOL closeLogFiles();
+	BOOL checkSelectedPixel(int x, int y, RECT *rect);
+
+	//* Variable Declarations
 	HDC hdc = NULL;
 
 	static HBITMAP hBitmap = NULL;
 	static PAINTSTRUCT ps;
 
 	static unsigned int resizedWindowWidth = 0, resizedWindowHeight = 0;
+	static int rectWidth = 960, rectHeight = 540;
 	static unsigned int pickedPixelX = 0, pickedPixelY = 0;
 	static BOOL bColorPickerLeftClick = FALSE;
+
+	TCHAR rgbValues[255];
+	RECT imageRect = { 25, 25, 25 + rectWidth, 25 + rectHeight };
 
 	// Code
 	switch (iMsg)
 	{
 		//* Message Handlers
 		case WM_CREATE:
-
-			gpFile_UserLog = fopen("User-Log.log", "w");
-			if (gpFile_UserLog == NULL)
-			{
-				MessageBox(hwnd, TEXT("Failed To Create User Log File ... Exiting !!!"), TEXT("Error"), MB_ICONERROR | MB_OK);
+			
+			if (createAndOpenLogFiles() == FALSE)
 				DestroyWindow(hwnd);
-			}
-			fprintf(gpFile_UserLog, "User Log File Created Successfully ...\n");
-
-			gpFile_ColorPickerLog = fopen("Color-Picker-Log.log", "w");
-			if (gpFile_ColorPickerLog == NULL)
-			{
-				MessageBox(hwnd, TEXT("Failed To Create Color Picker Log File ... Exiting !!!"), TEXT("Error"), MB_ICONERROR | MB_OK);
-				DestroyWindow(hwnd);
-			}
 
 			ZeroMemory((void**)&ps, sizeof(PAINTSTRUCT));
 
@@ -111,6 +108,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 				MessageBox(hwnd, TEXT("Failed To Load Bitmap ... Exiting !!!"), TEXT("Bitmap Error"), MB_ICONERROR | MB_OK);
 				DestroyWindow(hwnd);
 			}
+
 		break;
 
 
@@ -124,13 +122,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 				hdcMem = CreateCompatibleDC(hdc);
 				GetObject(hBitmap, sizeof(BITMAP), &bitmap);
 				SelectObject(hdcMem, hBitmap);
+
 				SetStretchBltMode(hdc, COLORONCOLOR);
 				StretchBlt(
 					hdc, 
-					0, 
-					0, 
-					resizedWindowWidth,
-					resizedWindowHeight,
+					imageRect.left, 
+					imageRect.top, 
+					imageRect.right - imageRect.left,
+					imageRect.bottom - imageRect.top,
 					hdcMem, 
 					0, 
 					0, 
@@ -142,7 +141,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 				if (bColorPickerLeftClick)
 				{
 					COLORREF pickedPixel = GetPixel(hdc, pickedPixelX, pickedPixelY);
-					fprintf(gpFile_ColorPickerLog, "R : %u, G : %u, B : %u\n", GetRValue(pickedPixel), GetGValue(pickedPixel), GetBValue(pickedPixel));
+					
+					unsigned int rValue = GetRValue(pickedPixel);
+					unsigned int gValue = GetGValue(pickedPixel);
+					unsigned int bValue = GetBValue(pickedPixel);
+
+					// fprintf(gpFile_ColorPickerLog, "R : %u, G : %u, B : %u\n", rValue, gValue, bValue);
+
+					RECT colorRect = { 450, 580, 530, 660 };
+					HBRUSH hBrush = CreateSolidBrush(RGB(rValue, gValue, bValue));
+					FillRect(hdc, &colorRect, hBrush);
+
+					wsprintf(rgbValues, "R : %u, G : %u, B : %u\n", rValue, gValue, bValue);
+					SetBkColor(hdc, RGB(0, 0, 0));
+					SetTextColor(hdc, RGB(0, 255, 0));
+
+					SetTextColor(hdc, RGB(0, 0, 0)); 
+					SetBkMode(hdc, TRANSPARENT);
+					TextOut(hdc, 420, 680, rgbValues, strlen(rgbValues));
+
 					bColorPickerLeftClick = FALSE;
 				}
 				
@@ -164,10 +181,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		case WM_LBUTTONDOWN:
 			
 			//! For Color Picking
-			bColorPickerLeftClick = TRUE;
 			pickedPixelX = GET_X_LPARAM(lParam);
 			pickedPixelY = GET_Y_LPARAM(lParam);
-			InvalidateRect(hwnd, NULL, TRUE);
+
+			if (checkSelectedPixel(pickedPixelX, pickedPixelY, &imageRect))
+			{
+				bColorPickerLeftClick = TRUE;
+				InvalidateRect(hwnd, NULL, TRUE);
+			}
 
 		break;
 
@@ -184,18 +205,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 				hBitmap = NULL;
 			}
 
-			if (gpFile_ColorPickerLog)
-			{
-				fclose(gpFile_ColorPickerLog);
-				gpFile_ColorPickerLog = NULL;
-			}
-
-			if (gpFile_UserLog)
-			{
-				fprintf(gpFile_UserLog, "Program Terminated Successfully ...\n");
-				fclose(gpFile_UserLog);
-				gpFile_UserLog = NULL;
-			}
+			closeLogFiles();
 
 			PostQuitMessage(0);
 
@@ -207,6 +217,68 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 
 	//* Calling Default Window Procedure
 	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
+BOOL checkSelectedPixel(int x, int y, RECT *rect)
+{
+	// Code
+	return (x >= rect->left && x <= rect->right && y >= rect->top && y <= rect->bottom);
+}
+
+BOOL createAndOpenLogFiles(void)
+{
+	//* Code
+	gpFile_UserLog = fopen("User-Log.log", "w");
+	if (gpFile_UserLog == NULL)
+	{
+		MessageBox(NULL, TEXT("Failed To Create User Log File ... Exiting !!!"), TEXT("Error"), MB_ICONERROR | MB_OK);
+		return FALSE;
+	}
+	fprintf(gpFile_UserLog, "Program Started Successfully ...\n");
+	fprintf(gpFile_UserLog, "User Log File Created Successfully ...\n");
+
+	gpFile_ColorPickerLog = fopen("Color-Picker-Log.log", "w");
+	if (gpFile_ColorPickerLog == NULL)
+	{
+		MessageBox(NULL, TEXT("Failed To Create Color Picker Log File ... Exiting !!!"), TEXT("Error"), MB_ICONERROR | MB_OK);
+		return FALSE;
+	}
+
+	gpFile_OGL_Log = fopen("OGL-Color.log", "w");
+	if (gpFile_ColorPickerLog == NULL)
+	{
+		MessageBox(NULL, TEXT("Failed To Create Color Picker Log File ... Exiting !!!"), TEXT("Error"), MB_ICONERROR | MB_OK);
+		return FALSE;
+	}
+
+	return TRUE;
+
+}
+
+BOOL closeLogFiles(void)
+{
+	//* Code
+	if (gpFile_OGL_Log)
+	{
+		fclose(gpFile_OGL_Log);
+		gpFile_OGL_Log = NULL;
+	}
+
+	if (gpFile_ColorPickerLog)
+	{
+		fclose(gpFile_ColorPickerLog);
+		gpFile_ColorPickerLog = NULL;
+	}
+
+	if (gpFile_UserLog)
+	{
+		fprintf(gpFile_UserLog, "Program Terminated Successfully ...\n");
+		fclose(gpFile_UserLog);
+		gpFile_UserLog = NULL;
+	}
+
+	return TRUE;
+
 }
 
 
