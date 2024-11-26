0bf84956f6d92bebc65e75d89ceeb1c432955509
AtharvNatu <natuatharv21@gmail.com>
2024-11-26 10:31:05 +0530

    Updated Go Automation Client

--- 02-COM/04-Automation/02-Clients/07-Go-Client/GoAutomationClient.go
+++ 02-COM/04-Automation/02-Clients/07-Go-Client/GoAutomationClient.go
@@ -8,7 +8,17 @@ import (
 
 func main() {
 
+	// Variable Declarations
+	var num1 int
+	var num2 int
+
 	// Code
+	fmt.Print("\nEnter Number 1 = ")
+	fmt.Scan(&num1)
+
+	fmt.Print("\nEnter Number 2 = ")
+	fmt.Scan(&num2)
+
 	ole.CoInitialize(0)
 	defer ole.CoUninitialize()
 
@@ -42,13 +52,29 @@ func main() {
 	}
 
 	var result *ole.VARIANT
-	result, err = dispatch_ptr.Invoke(dispid[0], ole.DISPATCH_METHOD, 2, 5)
+	result, err = dispatch_ptr.Invoke(dispid[0], ole.DISPATCH_METHOD, num1, num2)
 	if err != nil {
 		fmt.Println("Failed To Obtain Result For SumOfTwoIntegers() !!!")
 		return
 	}
 
 	sum := result.Value()
-	fmt.Println("Sum = ", sum)
+
+	dispid, err = dispatch_ptr.GetIDsOfName([]string{"SubtractionOfTwoIntegers"})
+	if err != nil {
+		fmt.Println("Failed To Obtain ID For SubtractionOfTwoIntegers() !!!")
+		return
+	}
+
+	result, err = dispatch_ptr.Invoke(dispid[0], ole.DISPATCH_METHOD, num1, num2)
+	if err != nil {
+		fmt.Println("Failed To Obtain Result For SubtractionOfTwoIntegers() !!!")
+		return
+	}
+
+	subtract := result.Value()
+
+	fmt.Printf("\nSum of %d and %d = %d\n", num1, num2, sum)
+	fmt.Printf("\nSubtraction of %d and %d = %d\n", num1, num2, subtract)
 
 }
