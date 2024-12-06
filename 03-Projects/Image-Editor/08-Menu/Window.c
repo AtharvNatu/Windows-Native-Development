580269f719062687a15f759ff849a6d1d239d6f1
AtharvNatu <natuatharv21@gmail.com>
2024-12-07 00:24:33 +0530

    Added Code For Menu Bar and Image File Dialog

---
+++ 03-Projects/Image-Editor/08-Menu/Window.c
@@ -0,0 +1,280 @@
+#include <windows.h>
+#include "Window.h"
+
+#define WINDOW_WIDTH 	800
+#define WINDOW_HEIGHT	600
+
+//* Global Callback Declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK ControlsDialogProc(HWND, UINT, WPARAM, LPARAM);
+
+//* Global Function Declarations
+OPENFILENAME OpenFileDialog(HWND hwndOwner);
+
+//* Entry-point Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	// Variable Declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyWindow");
+
+	//* Code
+	ZeroMemory((void*)&wndclass, sizeof(WNDCLASSEX));
+	
+	//* Initialization of Window Class
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = MAKEINTRESOURCE(IE_MENU);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(ADN_ICON));
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(ADN_ICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+	//* Register the above created class
+	RegisterClassEx(&wndclass);
+
+	//* Create Window in Memory
+	hwnd = CreateWindow(
+		szAppName,
+		TEXT("Atharv Natu : Menu Options"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//* Message Loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//* Variable Declarations
+	RECT rc;
+	PAINTSTRUCT ps;
+
+	HDC hdc = NULL;
+	HINSTANCE hInst = NULL;
+
+	static HBITMAP hBitmap = NULL;
+	static BOOL bImageLoaded = FALSE;
+	static TCHAR szImagePath[_MAX_PATH];
+	static unsigned int resizedWindowWidth = 0, resizedWindowHeight = 0;
+	
+	// Code
+	switch (iMsg)
+	{
+		//* Message Handlers
+		case WM_CREATE:
+			hInst = (HINSTANCE)((LPCREATESTRUCT)lParam)->hInstance;
+			ZeroMemory(&ps, sizeof(PAINTSTRUCT));
+		break;
+
+		case WM_SIZE:
+			resizedWindowWidth = LOWORD(lParam);
+			resizedWindowHeight = HIWORD(lParam);
+		break;
+
+		case WM_PAINT:
+			
+			HDC hdcMem;
+			BITMAP bitmap;
+
+			GetClientRect(hwnd, &rc);
+			hdc = BeginPaint(hwnd, &ps);
+			{
+				if (!bImageLoaded)
+				{
+					SetBkColor(hdc, RGB(0, 0, 0));
+					SetTextColor(hdc, RGB(0, 255, 0));
+					DrawText(hdc, "Click On File Menu And Select 'Open' To Open An Image File", -1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+				}
+				else
+				{
+					hdcMem = CreateCompatibleDC(hdc);
+					GetObject(hBitmap, sizeof(BITMAP), &bitmap);
+					SelectObject(hdcMem, hBitmap);
+					SetStretchBltMode(hdc, COLORONCOLOR);
+					StretchBlt(
+						hdc,
+						0,
+						0,
+						resizedWindowWidth,
+						resizedWindowHeight,
+						hdcMem,
+						0,
+						0,
+						bitmap.bmWidth,
+						bitmap.bmHeight,
+						SRCCOPY
+					);
+					if (hdcMem)
+					{
+						DeleteDC(hdcMem);
+						hdcMem = NULL;
+					}
+				}
+			}
+			if (hdc)
+			{
+				EndPaint(hwnd, &ps);
+				hdc = NULL;
+			}
+		break;
+
+		case WM_COMMAND:
+			switch(LOWORD(wParam))
+			{
+				case IDM_OPEN:
+					
+					OPENFILENAME ofn = OpenFileDialog(hwnd);
+					if (GetOpenFileName((LPOPENFILENAME)&ofn) == FALSE)
+					{
+						MessageBox(NULL, TEXT("Failed To Open File Dialog ... Exiting !!!"), TEXT("Error"), MB_OK | MB_ICONERROR);
+						DestroyWindow(hwnd);
+					}
+					wsprintf(szImagePath, ofn.lpstrFile);
+
+					if (bImageLoaded)
+					{
+						if (hBitmap)
+						{
+							DeleteObject(hBitmap);
+							hBitmap = NULL;
+						}
+					}
+					
+					hBitmap = (HBITMAP)LoadImage(NULL, szImagePath, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+					if (hBitmap == NULL)
+					{
+						bImageLoaded = FALSE;
+						MessageBox(NULL, TEXT("Failed To Load Bitmap ... Exiting !!!"), TEXT("Error"), MB_OK | MB_ICONERROR);
+						DestroyWindow(hwnd);
+					}
+					bImageLoaded = TRUE;
+					InvalidateRect(hwnd, NULL, TRUE);
+
+				break;
+
+				case IDM_EXIT:
+					DestroyWindow(hwnd);
+				break;
+
+				case IDM_EDIT:
+					DialogBox(hInst, MAKEINTRESOURCE(IE_DLG), hwnd, ControlsDialogProc);
+				break;
+
+				case IDM_ABOUT:
+				break;
+			}
+		break;
+
+		case WM_DESTROY:
+			
+			if (hBitmap)
+			{
+				DeleteObject(hBitmap);
+				hBitmap = NULL;
+			}
+
+			PostQuitMessage(0);
+
+		break;
+
+		default:
+		break;
+	}
+
+	//* Calling Default Window Procedure
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
+
+INT_PTR CALLBACK ControlsDialogProc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//*Variable Declarations
+	HWND hwndParent;
+	RECT rc;
+
+	// Code
+	switch(iMsg)
+	{
+		case WM_INITDIALOG:
+			return (INT_PTR)TRUE;
+
+		case WM_COMMAND:
+			switch(LOWORD(wParam))
+			{
+				case IDOK:
+				case IDCANCEL:
+					EndDialog(hDlg, (INT_PTR)0);
+				break;
+			}
+		return (INT_PTR)TRUE;
+
+		case WM_CLOSE:
+			EndDialog(hDlg, (INT_PTR)0);
+		break;
+
+		default:
+			return (INT_PTR)FALSE;
+	}
+
+	return (INT_PTR)FALSE;
+}
+
+OPENFILENAME OpenFileDialog(HWND hwndOwner)
+{
+	// Code
+	OPENFILENAME ofn;
+	TCHAR szFileName[_MAX_PATH];
+
+	
+	ZeroMemory(&ofn, sizeof(OPENFILENAME));
+
+	szFileName[0] = '\0';
+
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = hwndOwner;
+	ofn.hInstance = NULL;
+	ofn.lpstrFilter = "Image Files(.jpg|.png|.bmp|.jpeg)\0*.jpg;*.png;*.bmp;*.jpeg\0\0";
+	ofn.lpstrCustomFilter = NULL;
+	ofn.nMaxCustFilter = 0;
+	ofn.nFilterIndex = 1;
+	ofn.lpstrFile = szFileName;
+	ofn.nMaxFile = _MAX_PATH;
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = _MAX_FNAME + _MAX_EXT;
+	ofn.lpstrInitialDir = NULL;
+	ofn.lpstrTitle = TEXT("Select Image File");
+	ofn.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_READONLY | OFN_EXPLORER;
+	ofn.nFileOffset = 0;
+	ofn.nFileExtension = 0;
+	ofn.lpstrDefExt = TEXT("*");
+	ofn.lCustData = 0L;
+	ofn.lpTemplateName = NULL;
+
+	return ofn;
+}
---
+++ 03-Projects/Image-Editor/08-Menu/Window.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define ADN_ICON    101
+
+// Menu
+#define IE_MENU       102
+#define IDM_OPEN      103
+#define IDM_EXIT      104
+#define IDM_EDIT      105
+#define IDM_ABOUT     106
+
+// Dialog
+#define IE_DLG        107
+
